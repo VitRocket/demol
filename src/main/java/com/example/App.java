26e4e79723cb9ed429e3dc5f66e305c678ba6934
VitRocket <vitrocket@gmail.com>
2018-05-02 17:21:37 +0300

    added bootstrap 4, modified index page

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,9 +2,32 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class App {
+
+    @Bean
+    WebMvcConfigurer configurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/bootstrap/**")
+                        .addResourceLocations("classpath:/META-INF/resources/webjars/bootstrap/")
+                        .setCacheControl(CacheControl.maxAge(30L, TimeUnit.DAYS).cachePublic());
+
+                registry.addResourceHandler("/jquery/**")
+                        .addResourceLocations("classpath:/META-INF/resources/webjars/jquery/")
+                        .setCacheControl(CacheControl.maxAge(30L, TimeUnit.DAYS).cachePublic());
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
